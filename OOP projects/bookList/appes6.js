ceb67bef579a976dd76497837db546a1a836c55c
Tzur1234 <113801007+Tzur1234@users.noreply.github.com>
2022-12-21 20:07:09 +0200

    OOP projects in JavaScript
    Practice both ES6 and ES5 
    Practice prototype and inheritance

---
+++ OOP projects/bookList/appes6.js
@@ -0,0 +1,105 @@
+class Book{
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  addBook(book) {
+    // get the row element
+    const tbody = document.getElementById("book-list");
+
+    // creat a new element
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<th>${book.title}</th> <th>${book.author}</th> <th>${book.isbn}</th> <th> <a href="#" class="delete">X</a></th>`;
+
+    // Append new tr tbody
+    tbody.appendChild(tr);
+  }
+
+  deleteInput() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  }
+  showAlert(msg, className) {
+    // create div element
+    const div = document.createElement("div");
+
+    //Add the message to div
+    div.appendChild(document.createTextNode(msg, className));
+
+    //add className to div
+    div.className = `alert ${className}`;
+
+    // Get the primery div
+    const container = document.querySelector(".container");
+
+    //Get the form Element
+    const form = document.querySelector("#book-form");
+
+    //
+    console.log(container, form);
+
+    // add the alert message
+    container.insertBefore(div, form);
+
+    // remove div
+    setTimeout(function () {
+      document.querySelector(".alert").remove();
+    }, 2500);
+  }
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+// Add event "delegation" for deleting book
+document.getElementById("table").addEventListener("click", function (e) {
+    const ui = new UI()
+    
+  if (e.target.className === "delete") {
+    e.target.parentElement.parentElement.remove();
+    ui.showAlert('Remove complited !', 'success');
+  }
+  e.preventDefault();
+});
+
+// Add event for submit form
+document.getElementById("book-form").addEventListener("submit", function (e) {
+    // Get the values
+  
+    const title = document.getElementById("title"),
+      author = document.getElementById("author"),
+      isbn = document.getElementById("isbn");
+  
+    // Create UI object
+    const ui = new UI();
+  
+    
+  
+    // Validation
+    if (title.value === "" || author.value === "" || isbn.value === "") {
+          // show alert
+          ui.showAlert(`You must fill all the feilds !`, 'error');
+    } 
+    else {
+      // Create book object
+      const book = new Book(title.value, author.value, isbn.value);
+  
+      // Called UI AddBook function:
+      ui.addBook(book);
+  
+      // show alert
+      ui.showAlert(`Book was added !`, 'success');
+  
+      // Cleat input fileds
+      ui.deleteInput();
+    }
+  
+    e.preventDefault();
+  });
---
+++ OOP projects/bookList/readme.md
@@ -0,0 +1,12 @@
+# booklist - ES5
+
+in this project I have built an application that add each givven task
+to table. Unlike tasks list, this project I have used class to represent the book details and and store all the functions that reponsible for the UI in UI object
+
+The UI object has several prototype, each of them has a function
+- UI.prototype.showAlert    show alert (error or success)
+- UI.prototype.deleteInput  to delete input fields for each submition
+- UI.prototype.addBook      that responsible for adding new book to the table
+
+The main goal of this project is to practice the usage of classes with ES5
+
